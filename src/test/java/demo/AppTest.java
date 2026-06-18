ed68a49b1576ce15d92a5a5e1325c7e1b48b7f74
VkVom <sathishpoojaryannu1@gmail.com>
2026-06-18 11:24:46 +0530

    add to hub

---
+++ src/test/java/demo/AppTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.junit.jupiter.api.Test; 
+import static org.junit.jupiter.api.Assertions.assertEquals;  
+public class AppTest {  
+@Test 
+public void testAdd() { 
+App app = new App(); 
+int result = app.add(2, 3); 
+System.out.println("Running test: 2 + 3 = " + result); 
+assertEquals(5, result);  
+} 
+} 
